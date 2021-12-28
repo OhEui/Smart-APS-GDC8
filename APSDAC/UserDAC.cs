bf6fe546c024d87988a1c14335e059465636a5a6
enterpro75 <enterpro75@gmail.com>
2021-12-28 10:03:18 +0900

    UserDAC 수정
    UserDAC 수정

--- APSDAC/UserDAC.cs
+++ APSDAC/UserDAC.cs
@@ -33,46 +33,99 @@ namespace APSDAC
         /*
          UserLogin: 유저가 로그인하는동안 유지되는 정보
          UserInfo: 유저의 계정 정보
+
+            winform 구현 우선순위
+          1순위 Login, CheckID, CheckEmail, SignUp
+          2순위 Logout
+          3순위 ResetPasswort, UpdateUserInfo, WithDraw
          */
 
-        public UserLogin Login(string id, string password) 
+        public WebMessage<UserLogin> Login(string id, string password) 
         {
-            throw new NotImplementedException();
+            return new WebMessage<UserLogin>()
+            {
+                IsSuccess = true,
+                ResultMessage = "로그인에 성공하였습니다.",
+                Data = new UserLogin() 
+                {
+                    User_ID = "test",
+                    User_NO = 1,
+                    User_Name = "홍길동",
+                    Dept_ID = 1,
+                    Dept_Name = "영업부"
+                }
+            };
         }
 
         public WebMessage Logout(UserLogin data)
         {
-            throw new NotImplementedException();
+            return new WebMessage() 
+            {
+                IsSuccess = true,
+                ResultMessage = "로그아웃에 성공하였습니다."
+            };
         }
 
         public WebMessage CheckID(string id)
         {
-            throw new NotImplementedException();
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "사용가능한 ID입니다."
+            };
         }
 
         public WebMessage CheckEmail(string email)
         {
-            throw new NotImplementedException();
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "사용가능한 Email입니다."
+            };
         }
 
         public WebMessage SignUp(UserInfo info)
         {
-            throw new NotImplementedException();
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "회원가입에 성공하였습니다."
+            };
         }
 
         public WebMessage ResetPasswort(string id, string email) 
         {
-            throw new NotImplementedException();
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "임시비밀번호를 계정과 연결된 이메일로 전송하였습니다."
+            };
         }
 
-        public WebMessage UpdateUserInfo(UserInfo info)
+        public WebMessage<UserLogin> UpdateUserInfo(UserInfo info)
         {
-            throw new NotImplementedException();
+            return new WebMessage<UserLogin>()
+            {
+                IsSuccess = true,
+                ResultMessage = "회원정보를 수정하였습니다.",
+                Data = new UserLogin()
+                {
+                    User_ID = "test",
+                    User_NO = 1,
+                    User_Name = "홍길동",
+                    Dept_ID = 1,
+                    Dept_Name = "영업부"
+                }
+            };
         }
 
         public WebMessage WithDraw(UserLogin data)
         {
-            throw new NotImplementedException();
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "회원탈퇴를 하였습니다."
+            };
         }
     }
 }
--- APSVO/User/UserInfo.cs
+++ APSVO/User/UserInfo.cs
@@ -8,5 +8,16 @@ namespace APSVO
 {
     public class UserInfo
     {
+        // 회원가입 시 입력하는 항목들
+        public string User_ID { get; set; }
+        public string User_PWD { get; set; }
+        public string User_Name { get; set; }
+        public string User_phone { get; set; }
+        public string User_Birth { get; set; }
+        public int Dept_ID { get; set; }
+
+        // 회원가입 시 입력하면 안되는 항목들
+        public int? User_NO { get; set; }
+        public bool User_IsAdmin { get; set; }
     }
 }
