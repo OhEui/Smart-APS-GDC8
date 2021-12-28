ce8e677b3ef790cd0889f3895e254a35414f097a
enterpro75 <enterpro75@gmail.com>
2021-12-28 12:53:24 +0900

    UserDAC 수정

--- APSDAC/UserDAC.cs
+++ APSDAC/UserDAC.cs
@@ -40,25 +40,43 @@ namespace APSDAC
           3순위 ResetPasswort, UpdateUserInfo, WithDraw
          */
 
-        public WebMessage<UserLogin> Login(string id, string password) 
+        public WebMessage<UserLogin> Login(string id, string password)
         {
-            return new WebMessage<UserLogin>()
+            WebMessage<UserLogin> msg = new WebMessage<UserLogin>();
+            UserLogin login = null;
+
+            string sql = @"";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
             {
-                IsSuccess = true,
-                ResultMessage = "로그인에 성공하였습니다.",
-                Data = new UserLogin() 
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
                 {
-                    User_ID = "test",
-                    User_NO = 1,
-                    User_Name = "홍길동",
-                    Dept_ID = 1,
-                    Dept_Name = "영업부"
+                    login = Helper.DataReaderMapToList<UserLogin>(reader)?.FirstOrDefault();
+
+                    if (login != null)
+                    {
+                        msg.IsSuccess = true;
+                        msg.ResultMessage = "로그인에 성공하였습니다.";
+                        msg.Data = login;
+                    }
+                    else 
+                    {
+                        msg.IsSuccess = false;
+                        msg.ResultMessage = "로그인 중 오류가 발생하였습니다.";
+                        msg.Data = null;
+                    }
                 }
-            };
+            }
+            return msg;
         }
 
         public WebMessage Logout(UserLogin data)
         {
+            // 세션, 쿠키등이 있으면 해제 작업을 함
+
             return new WebMessage() 
             {
                 IsSuccess = true,
@@ -68,33 +86,79 @@ namespace APSDAC
 
         public WebMessage CheckID(string id)
         {
-            return new WebMessage()
+            WebMessage msg = new WebMessage();
+            string sql = @"";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
             {
-                IsSuccess = true,
-                ResultMessage = "사용가능한 ID입니다."
-            };
+                cmd.Parameters.AddWithValue("@ID", id);
+
+                int iResult = cmd.ExecuteNonQuery();
+                if (iResult > 0)
+                {
+                    msg.IsSuccess = false;
+                    msg.ResultMessage = "사용 중인 ID입니다.";
+                }
+                else
+                {
+                    msg.IsSuccess = true;
+                    msg.ResultMessage = "사용 가능한 ID입니다.";
+                }
+            }
+            return msg;
         }
 
         public WebMessage CheckEmail(string email)
         {
-            return new WebMessage()
+            WebMessage msg = new WebMessage();
+            string sql = @"";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
             {
-                IsSuccess = true,
-                ResultMessage = "사용가능한 Email입니다."
-            };
+                cmd.Parameters.AddWithValue("@Email", email);
+
+                int iResult = cmd.ExecuteNonQuery();
+                if (iResult > 0)
+                {
+                    msg.IsSuccess = false;
+                    msg.ResultMessage = "사용 중인 Email입니다.";
+                }
+                else
+                {
+                    msg.IsSuccess = true;
+                    msg.ResultMessage = "사용 가능한 Email입니다.";
+                }
+            }
+            return msg;
         }
 
         public WebMessage SignUp(UserInfo info)
         {
-            return new WebMessage()
+            WebMessage msg = new WebMessage();
+            string sql = @"";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
             {
-                IsSuccess = true,
-                ResultMessage = "회원가입에 성공하였습니다."
-            };
+                //cmd.Parameters.AddWithValue("@Email", email);
+                
+                int iResult = cmd.ExecuteNonQuery();
+                if (iResult > 0)
+                {
+                    msg.IsSuccess = true;
+                    msg.ResultMessage = "회원가입을 성공하였습니다.";
+                }
+                else
+                {
+                    msg.IsSuccess = false;
+                    msg.ResultMessage = "회원가입 중 오류가 발생하였습니다.";
+                }
+            }
+            return msg;
         }
 
         public WebMessage ResetPasswort(string id, string email) 
         {
+            // 임시비밀번호 생성 -> DB에 저장 -> 이메일 전송
             return new WebMessage()
             {
                 IsSuccess = true,
@@ -104,19 +168,30 @@ namespace APSDAC
 
         public WebMessage<UserLogin> UpdateUserInfo(UserInfo info)
         {
-            return new WebMessage<UserLogin>()
+            WebMessage<UserLogin> msg = new WebMessage<UserLogin>();
+            string sql = @"";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
             {
-                IsSuccess = true,
-                ResultMessage = "회원정보를 수정하였습니다.",
-                Data = new UserLogin()
+                //cmd.Parameters.AddWithValue("@Email", email);
+
+                int iResult = cmd.ExecuteNonQuery();
+                if (iResult > 0)
                 {
-                    User_ID = "test",
-                    User_NO = 1,
-                    User_Name = "홍길동",
-                    Dept_ID = 1,
-                    Dept_Name = "영업부"
+                    msg.IsSuccess = true;
+                    msg.ResultMessage = "회원가입을 성공하였습니다.";
+                    msg.Data = new UserLogin()
+                    {
+                    };
                 }
-            };
+                else
+                {
+                    msg.IsSuccess = false;
+                    msg.ResultMessage = "회원가입 중 오류가 발생하였습니다.";
+                    msg.Data = null;
+                }
+            }
+            return msg;
         }
 
         public WebMessage WithDraw(UserLogin data)
@@ -127,5 +202,57 @@ namespace APSDAC
                 ResultMessage = "회원탈퇴를 하였습니다."
             };
         }
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        public WebMessage<UserLogin> LoginTest(string id, string password)
+        {
+            return new WebMessage<UserLogin>()
+            {
+                IsSuccess = true,
+                ResultMessage = "로그인에 성공하였습니다.",
+                Data = new UserLogin()
+                {
+                    User_ID = "test",
+                    User_NO = 1,
+                    User_Name = "홍길동",
+                    Dept_ID = 1,
+                    Dept_Name = "영업부"
+                }
+            };
+        }
+        public WebMessage LogoutTest(UserLogin data)
+        {
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "로그아웃에 성공하였습니다."
+            };
+        }
+        public WebMessage CheckIDTest(string id)
+        {
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "사용 가능한 ID입니다.."
+            };
+        }
+        public WebMessage CheckEmailTest(string email)
+        {
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "사용 가능한 Email입니다."
+            };
+        }
+        public WebMessage SignUpTest(UserInfo info)
+        {
+            return new WebMessage()
+            {
+                IsSuccess = true,
+                ResultMessage = "회원가입에 성공하였습니다."
+            };
+        }
     }
+
+
 }
---
+++ APSWinForm/Util/ExcelUtil.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace APSWinForm
+{
+    public class ExcelUtil
+    {
+        public void ExcelExport(DataGridView dgv)
+        {
+            //저장할 디렉토리, 파일명을 물어보고, 그 파일경로로 엑셀파일 저장
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Excel Files(*.xls)|*.xls";
+            dlg.Title = "엑셀파일로 내보내기";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkBook = xlApp.Workbooks.Add();
+                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                DataTable dt = (DataTable)dgv.DataSource;
+
+                for (int c = 0; c < dt.Columns.Count; c++)
+                {
+                    xlWorkSheet.Cells[1, c + 1] = dt.Columns[c].ColumnName;
+                }
+
+                for (int r = 0; r < dt.Rows.Count; r++)
+                {
+                    for (int c = 0; c < dt.Columns.Count; c++)
+                    {
+                        xlWorkSheet.Cells[r + 2, c + 1] = dt.Rows[r][c].ToString();
+                    }
+                }
+
+                xlWorkBook.SaveAs(dlg.FileName, Excel.XlFileFormat.xlWorkbookNormal);
+                xlWorkBook.Close(true);
+                xlApp.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlApp);
+
+                MessageBox.Show("엑셀 다운로드 완료");
+            }
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
