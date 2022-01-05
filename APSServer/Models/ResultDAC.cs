eaf9f50265eb3a3917a069862f5ef59e23bebaf6
enterpro75 <enterpro75@gmail.com>
2022-01-05 15:20:58 +0900

    차트 수정

---
+++ APSServer/Models/ResultDAC.cs
@@ -0,0 +1,76 @@
+﻿using APSVO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace APSServer.Models
+{
+    public class ResultDAC : IDisposable
+    {
+        SqlConnection conn = null;
+
+        public ResultDAC()
+        {
+            string strConn = APSEncrypt.AES256.Decrypt(WebConfigurationManager.ConnectionStrings["DBInfo"].ConnectionString);
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+
+        public void Dispose()
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+                conn = null;
+            }
+        }
+
+        public List<ChartData> GetEQPGanttData()
+        {
+            List<EQPGanttData> data = new List<EQPGanttData>();
+            List<ChartData> result = new List<ChartData>();
+
+            string sql = @"
+select EQP_ID, START_TIME, END_TIME, LOT_ID, MACHINE_STATE, STEP_ID
+from EQP_PLAN";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    data = Helper.DataReaderMapToList<EQPGanttData>(reader);
+                }
+            }
+
+            foreach (var item in data)
+            {
+                result.Add(new ChartData()
+                {
+                    category = item.EQP_ID,
+                    fromDate = item.START_TIME.ToString("yyyy-mm-dd hh:mm:ss"),
+                    toDate = item.END_TIME.ToString("yyyy-mm-dd hh:mm:ss"),
+                    task = item.MACHINE_STATE == "SETUP" ? item.MACHINE_STATE : item.LOT_ID,
+                    colorIdx = GetColorIdx(item.STEP_ID, item.MACHINE_STATE),
+                    brighten = 0.4
+                });
+            }
+            return result;
+        }
+
+        private int GetColorIdx(string step_id, string machine_state)
+        {
+            if (machine_state == "SETUP") return 5;
+            switch (step_id) 
+            {
+                case "PRESS": return 1;
+                case "PAINT": return 2;
+                case "FINISH": return 3;
+            }
+            return 0;
+        }
+    }
+}
---
+++ APSVO/Chart/ChartData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSVO
+{
+    /*
+select EQP_ID 'category', CONVERT(NCHAR(19),START_TIME,20) 'fromDate', CONVERT(NCHAR(19),END_TIME,20) 'toDate',
+case MACHINE_STATE when 'SETUP' 
+	then MACHINE_STATE
+	else LOT_ID 
+end 'task' 
+from EQP_PLAN
+     */
+    public class ChartData
+    {
+    
+        public string category { get; set; } // y축 데이터 
+        public string fromDate { get; set; } // "2018-01-01 15:30"
+        public string toDate { get; set; } // "2018-01-01 15:30"
+        public string task { get; set; } // bar위에 표시될 텍스트
+        public int colorIdx { get; set; } = 0; // amchart colorset에서 color 가져올 때 사용하는 index값
+        public double brighten { get; set; } = 0.4; // 0~1 사이의 값만 넣어야 함
+    }
+}
