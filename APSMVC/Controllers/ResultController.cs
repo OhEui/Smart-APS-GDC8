e050c740f1f7d394577aee28cb4c9b5c0c401f09
enterpro75 <enterpro75@gmail.com>
2022-01-05 10:26:08 +0900

    MVC 프로젝트 수정

---
+++ APSMVC/Controllers/ResultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APSMVC.Controllers
+{
+    /*
+        EQP Gantt, 가동률 분석
+        ResultController는 나중에 하나의 파일로 합칠 예정
+     */
+    public partial class ResultController : Controller
+    {
+        // GET: Result
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
